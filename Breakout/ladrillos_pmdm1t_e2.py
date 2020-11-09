5ca637545adfba3e8f5d78d415023541de3eb417
Manuel Fernández Navarro <55835494+ManuelFN@users.noreply.github.com>
2020-11-09 09:58:48 +0100

    4.0
    Solucionado el problema con el rebote de la bola con la pala del jugador y añadido que cuando llegue a 0 vidas le salte en la pantalla "HAS PERDIDO" y "Presiona R para reiniciar", si presiona la tecla R empezará de nuevo el juego teniendo de vuelta las 3 vidas.

--- Breakout/ladrillos_pmdm1t_e2.py
+++ Breakout/ladrillos_pmdm1t_e2.py
@@ -25,6 +25,7 @@ AZUL = (0, 0, 255)
 AZUL_OSCURO = (36, 90, 190)
 H_50D2FE = (94, 210, 254)
 
+
 class Pelota:
     def __init__(self, fichero_imagen):
         # --- Atributos de la Clase ---
@@ -61,10 +62,36 @@ class Pelota:
             self.dir_y = -self.dir_y
             pygame.mixer.Sound.play(sonido)
         if self.y + self.alto >= ALTO:
-            # self.dir_y = -self.dir_y
+            global vidas
+            # Restamos una vida
+            vidas -= 1
+            # Si llega a 0 le imprimimos que ha perdido y si quiere volver a intentarlo que presione la tecla 'R'
+            if vidas == 0:
+                # Creamos los tipos de fuente para los letreros que vamos a poner
+                font = pygame.font.Font(None, 74)
+                font2 = pygame.font.Font(None, 34)
+                # Creamos los letreros con sus respectivos tamaños y posiciones
+                text = font.render("HAS PERDIDO", 1, BLANCO)
+                texto2 = font2.render("Presiona R para reiniciar", 1, BLANCO)
+                text_rect = text.get_rect(center=(ANCHO / 2, ALTO / 2))
+                # Los imprimos por pantalla
+                pantalla.blit(texto2, (263, 325))
+                pantalla.blit(text, text_rect)
+                pygame.display.flip()
+
+                # Realizamos una comprobación al jugador de si quiere reiniciar el juego tras haber perdido
+
+                while True:
+                    for event in pygame.event.get():
+                        if event.type == QUIT:
+                            pygame.quit()
+                            sys.exit()
+                        if event.type == KEYDOWN and event.key == K_r:
+                            vidas = 4
+                            return
+
             self.reiniciar()
             pygame.mixer.Sound.play(sonido)
-
 
     def reiniciar(self):
         self.x = int(ANCHO / 2)
@@ -132,60 +159,98 @@ class Jugador(pygame.sprite.Sprite):
                 and pelota.y + pelota.alto > self.rect.y
                 and pelota.y < self.rect.y + 15
         ):
-            pelota.dir_x = -pelota.dir_x
             pelota.dir_y = -pelota.dir_y
 
 
-# Inicialización de Pygame, creación de la ventana, título y control de reloj.
-pygame.init()
+# class Ladrillos(pygame.sprite.Sprite):
+#     def __init__(self, color, width, height):
+#         super().__init__()
+#         self.image = pygame.Surface([width, height])
+#         self.image.fill(BLANCO)
+#         self.image.set_colorkey(BLANCO)
+#         pygame.draw.rect(self.image, color, [0, 0, width, height])
+#         self.rect = self.image.get_rect()
+#
+#         all_bricks = pygame.sprite.Group()
+#         for i in range(7):
+#             brick = Ladrillos(ROJO, 80, 30)
+#             brick.rect.x = 20 + i * 70
+#             brick.rect.y = 60
+#             sprites.add(brick)
+#             all_bricks.add(brick)
+#         for i in range(7):
+#             brick = Ladrillos(BLANCO, 80, 30)
+#             brick.rect.x = 20 + i * 70
+#             brick.rect.y = 100
+#             sprites.add(brick)
+#             all_bricks.add(brick)
+#         for i in range(7):
+#             brick = Ladrillos(VERDE, 80, 30)
+#             brick.rect.x = 20 + i * 70
+#             brick.rect.y = 140
+#             sprites.add(brick)
+#             all_bricks.add(brick)
 
-pantalla = pygame.display.set_mode((ANCHO, ALTO))
-pygame.display.set_caption("Destrozando ladrillos")
-clock = pygame.time.Clock()
+def main():
+    # Inicialización de Pygame, creación de la ventana, título y control de reloj.
+    pygame.init()
 
-pelota = Pelota("imagenes/ball.png")
+    global pantalla
 
-# Grupo de sprites, instanciación del objeto jugador.
-sprites = pygame.sprite.Group()
-pala = Jugador()
-sprites.add(pala)
+    pantalla = pygame.display.set_mode((ANCHO, ALTO))
 
-# Bucle de juego
-ejecutando = True
-while ejecutando:
+    pygame.display.set_caption("Destrozando ladrillos")
+    clock = pygame.time.Clock()
 
-    pelota.mover()
-    pelota.rebotar()
-    pala.golpear(pelota)
+    pelota = Pelota("imagenes/ball.png")
 
-    # Especifica la velocidad del bucle de juego
-    clock.tick(FPS)
+    # Grupo de sprites, instanciación del objeto jugador.
+    sprites = pygame.sprite.Group()
+    pala = Jugador()
+    sprites.add(pala)
 
-    # Eventos
-    for event in pygame.event.get():
-        # Se cierra y termina el bucle
-        if event.type == pygame.QUIT:
-            ejecutando = False
+    # Bucle de juego
+    ejecutando = True
+    while ejecutando:
 
-    # Actualización de sprites
-    sprites.update()
+        pelota.mover()
+        pelota.rebotar()
+        pala.golpear(pelota)
 
-    # Fondo de pantalla, dibujo de sprites y formas geométricas.
-    pantalla.fill(NEGRO)
-    sprites.draw(pantalla)
-    pantalla.blit(pelota.imagen, (round(pelota.x), round(pelota.y)))
-    pygame.draw.line(pantalla, H_50D2FE, (400, 0), (400, 800), 1)
-    pygame.draw.line(pantalla, VERDE, (0, 300), (800, 300), 1)
-    icono = pygame.image.load("imagenes/pong_icono.png")
-    pygame.display.set_icon(icono)
+        # Especifica la velocidad del bucle de juego
+        clock.tick(FPS)
 
-    font = pygame.font.Font(None, 34)
-    text = font.render("Puntuación: " + str(puntuacion), 1, BLANCO)
-    pantalla.blit(text, (20, 10))
-    text = font.render("Vidas: " + str(vidas), 1, BLANCO)
-    pantalla.blit(text, (650, 10))
+        # Eventos
+        for event in pygame.event.get():
+            # Se cierra y termina el bucle
+            if event.type == pygame.QUIT:
+                ejecutando = False
 
-    # Actualiza el contenido de la pantalla.
-    pygame.display.flip()
+        # Actualización de sprites
+        sprites.update()
 
-pygame.quit()
+        # Fondo de pantalla, dibujo de sprites y formas geométricas.
+        pantalla.fill(NEGRO)
+        sprites.draw(pantalla)
+        pantalla.blit(pelota.imagen, (round(pelota.x), round(pelota.y)))
+        pygame.draw.line(pantalla, H_50D2FE, (400, 0), (400, 800), 1)
+        pygame.draw.line(pantalla, VERDE, (0, 300), (800, 300), 1)
+        icono = pygame.image.load("imagenes/pong_icono.png")
+        pygame.display.set_icon(icono)
+
+        # Imprimimos los textos de puntuación y vidas
+
+        font = pygame.font.Font(None, 34)
+        text = font.render("Puntuación: " + str(puntuacion), 1, BLANCO)
+        pantalla.blit(text, (20, 10))
+        text = font.render("Vidas: " + str(vidas), 1, BLANCO)
+        pantalla.blit(text, (650, 10))
+
+        # Actualiza el contenido de la pantalla.
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
